53930701b6af817d20d2dfe17fd88a8e62643f05
Timothy Bramlett <timothy.bramlett@gmail.com>
2020-02-03 10:17:30 -0600

    Update install_and_setup_zsh.sh

--- install_and_setup_zsh.sh
+++ install_and_setup_zsh.sh
@@ -1,7 +1,13 @@
 
 # Installs and sets up zsh with the plugins I prefer
 sudo apt install zsh -y
+
 export RUNZSH=no
+export CHSH=no 
+
+echo $CHSH
+echo $RUNZSH
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> .zshrc
